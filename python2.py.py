2620b4c8a1aae811d77381900040e34df93180e7
surajharish <140801915+surajharish@users.noreply.github.com>
2023-07-28 15:01:21 +0530

    Add files via upload

---
+++ python2.py.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""
+
+val = int(input("Enter a value : "))
+str_val = str(val)
+if str_val == str_val[::-1]:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+    
+for i in range(10):
+    if str_val.count(str(i)) > 0:
+        print(str(i),"appears", str_val.count(str(i)), "times");
+
+
+# In[ ]:
+
+
+def bin2Dec(val):
+    rev=val[::-1]
+    dec = 0
+    i = 0
+    for dig in rev:
+        dec += int(dig) * 2**i
+        i += 1
+    
+    return dec
+
+def oct2Hex(val):
+    rev=val[::-1]
+    dec = 0
+    i = 0
+    for dig in rev:
+        dec += int(dig) * 8**i
+        i += 1
+    list=[]
+    while dec != 0:
+        list.append(dec%16)
+        dec = dec // 16
+        
+    nl=[]
+    for elem in list[::-1]:
+        if elem <= 9:
+            nl.append(str(elem))
+        else:
+            nl.append(chr(ord('A') + (elem -10)))
+    hex = "".join(nl)
+    
+    return hex
+
+num1 = input("Enter a binary number : ")    
+print(bin2Dec(num1))
+num2 = input("Enter a octal number : ")
+print(oct2Hex(num2))
+
+
+# In[ ]:
+
+
+sentence = input("Enter a sentence : ")
+
+wordList = sentence.split(" ")
+print("This sentence has", len(wordList), "words")
+
+digCnt = upCnt = loCnt = 0
+
+for ch in sentence:
+    if '0' <= ch <= '9':
+        digCnt += 1
+    elif 'A' <= ch <= 'Z':
+        upCnt += 1
+    elif 'a' <= ch <= 'z':
+        loCnt += 1
+
+print("This sentence has", digCnt, "digits", upCnt, "upper case letters", loCnt, "lower case letters")
+
+
+# In[ ]:
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 01:48:17 2023
+
+@author: Prabodh C P
+"""
+
+str1 = input("Enter String 1 \n")
+str2 = input("Enter String 2 \n")
+
+if len(str2) < len(str1):
+    short = len(str2)
+    long = len(str1)
+else:
+    short = len(str1)
+    long = len(str2)
+    
+
+matchCnt = 0
+for i in range(short):
+    if str1[i] == str2[i]:
+        matchCnt += 1
+
+print("Similarity between two said strings:")
+print(matchCnt/long)
+
