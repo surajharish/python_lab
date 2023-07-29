9dc110ed634983c0442af733aed79c75bf2d5468
surajharish <140801915+surajharish@users.noreply.github.com>
2023-07-29 21:30:13 +0530

    Add files via upload

---
+++ program 5 .py.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def isphonenumber(numStr):
+    if len(numStr) != 12:
+        return False
+    for i in range(len(numStr)):
+        if i==3 or i==7:
+            if numStr[i] != "-":
+                return False
+        else:
+            if numStr[i].isdigit() == False:
+                return False
+    return True
+def chkphonenumber(numStr):
+    ph_no_pattern = re.compile(r'^\d{3}-\d{3}-\d{4}$')
+    if ph_no_pattern.match(numStr):
+        return True
+    else:
+        return False
+
+ph_num = input("Enter a phone number : ")
+print("Without using Regular Expression")
+if isphonenumber(ph_num):
+    print("Valid phone number")
+else:
+    print("Invalid phone number")
+
+print("Using Regular Expression")
+if chkphonenumber(ph_num):
+    print("Valid phone number")
+else:
+    print("Invalid phone number")
+
+
+
+# In[ ]:
+
+
+import re
+# Define the regular expression for phone numbers
+phone_regex = re.compile(r'\+\d{12}')
+email_regex = re.compile(r'[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Z|a-z]{2,}')
+# Open the file for reading
+with open('example.txt', 'r') as f:
+ # Loop through each line in the file
+ for line in f:
+ # Search for phone numbers in the line
+ matches = phone_regex.findall(line)
+ # Print any matches found
+ for match in matches:
+ print(match)
+ matches = email_regex.findall(line)
+ # Print any matches found
+ for match in matches:
+ print(match) 
+
+
+# In[ ]:
+
+
+
+
